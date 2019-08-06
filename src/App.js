f7f59b603aa50629a408f184ccb64c68022f3f96
Fabian Spisa <snnoox@googlemail.com>
2019-08-06 15:10:08 +0200

    Changed App.js from class to functional

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import youtubeKEY from "./api/youtubeKEY"
 
 import { Grid } from "@material-ui/core";
 
-import {SearchBar, VideoList, VideoDetail} from "./components";
+import { SearchBar, VideoList, VideoDetail } from "./components";
 import youtube from "./api/youtube";
 
+const API_KEY = youtubeKEY;
 
-class App extends React.Component{
-    state = {videos:[],
-    selectedVideo:null}
-        componentDidMount(){
-            this.handleSubmit("Youtube");
-        }
-    handleSubmit = async (searchTerm) =>{
-        const res = await youtube.get('search',{
-        params:{
-            part:"snippet",
-            maxResults:5,
-            key: "AIzaSyAKgJEEbyT_c3iPZ9dziJwTWyVniPAkbmY",
-            q:searchTerm
-        }});
-    this.setState({videos: res.data.items, selectedVideo:res.data.items[0]})    
-    }
+const App = () => {
+  const [videos, setVideo] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-    onVideoSelect = (video) =>{
-        this.setState({selectedVideo: video})
-    }
-render(){
-    const {selectedVideo, videos} = this.state;
-  return (
-    <Grid justify="center" container spacing={10}>
-      <Grid item xs={12}>
-        <Grid container spacing={10}>
-          <Grid item xs={12}>
-          <SearchBar onFormSubmit={this.handleSubmit}/>
+  
+
+ const handleSubmit = async searchTerm => {
+    const res = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: API_KEY,
+        q: searchTerm
+      }
+    });
+    setVideo(res.data.items);
+    setSelectedVideo(res.data.items[0]);
+  };
+
+ const onVideoSelect = video => {
+      setSelectedVideo(video)
+   
+  };
+
+    return (
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail video={selectedVideo} />
+            </Grid>
+            <Grid item xs={4}>
+              <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+            </Grid>
           </Grid>
-          <Grid item xs={8}>
-            <VideoDetail video={selectedVideo}/>
-          </Grid>
-          <Grid item xs={4}>
-              <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
-              </Grid>
         </Grid>
       </Grid>
-    </Grid>
-
-  );
+    );
+  
 };
-}
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -24,7 +24,7 @@ class SearchBar extends React.Component {
     return (
       <Paper elevation={6} style={{ padding: "25px" }}>
         <form onSubmit={this.handleSubmit}>
-          <TextField fullWidth label="Search..." onChange={this.handleChange} />
+          <TextField fullWidth label="Videotitel suchen..." onChange={this.handleChange} />
         </form>
       </Paper>
     );
